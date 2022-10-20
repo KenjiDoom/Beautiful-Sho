fd8922dc99357238d00fcb675c8d6776d900ff61
KenjiDoom <77996865+KenjiDoom@users.noreply.github.com>
2022-10-19 17:03:04 -0700

    This chunk of hot mess
    haha, I was able to fix the amount of items in the city_data list problem. But I feel like I can make this code look better. I don't like all the lists I had to make. BUT it works so that's good!

--- sho.py
+++ sho.py
@@ -3,7 +3,6 @@ from rich.table import Table
 import shodan
 import sys
 
-# Main section of code Input & Calculate
 def main():
     # Import Shodan API Key
     SHODAN_API_KEY = ''
@@ -15,19 +14,35 @@ def main():
     banner_data = []
     ip_addr = []
     city_data = []
+    asn_data = []
+    org_data = []
+    domains_data = []
+    hostname_data = []
 
     for index, ip_search in enumerate(IP_LIST):
         host = api.host(str(ip_search))
-        city = host.get('city', 'n/a')
-        city_data.append(city)
-
         for items in host['data']:
             open_ports = str(items['port'])
             port_data.append(open_ports)
+            
             ip_addr.append(items['ip_str'])
             banner_data.append(str(items['data']))
 
-    #Formatting out here
+            city = host.get('city', 'n/a')
+            city_data.append(city)
+            
+            org = str(items['org'])
+            org_data.append(org)
+
+            asn = str(items['asn'])
+            asn_data.append(asn)
+
+            domains = str(items['domains'])
+            domains_data.append(domains)
+
+            hostname = str(items['hostnames'])
+            hostname_data.append(hostname)
+
     table = Table(title="Shodan Results")
     table = Table(show_lines=True)
 
@@ -40,8 +55,8 @@ def main():
     table.add_column("Domains", style="red")
     
     # Banner section
-    for index, (a, b, c) in enumerate(zip(ip_addr, port_data, banner_data)):
-        table.add_row(str(a), str(b), str(c), str(city_data))
+    for index, (a, b, c, d, e, f, g, h) in enumerate(zip(ip_addr, port_data, banner_data, city_data, asn_data, org_data, domains_data, hostname_data)):
+        table.add_row(str(a), str(b), str(c), str(d), str(e), str(f), str(g), str(h))
     
     console = Console()
     console.print(table)
