f18cb51221525a84873cf8b2dbbfef1c53920000
KenjiDoom <77996865+KenjiDoom@users.noreply.github.com>
2022-11-03 20:33:43 -0700

    Increased output data
    More information about an IP address is now displayed.

--- sho.py
+++ sho.py
@@ -7,7 +7,6 @@ import sys
 SHODAN_API_KEY = ''
 api = shodan.Shodan(SHODAN_API_KEY)
 
-
 class diagram:
     def __init__(self, *argv):
         for arg in argv:
@@ -15,27 +14,33 @@ class diagram:
 
     def shodan_search(self):
         host = api.host(self.IP)
-        self.ip, self.city, self.port = [],[],[]
+        self.ip, self.banner, self.port, self.city, self.domains, self.asn = [],[],[],[],[],[]
         for items in host['data']:
             self.ip.append(items['ip_str'])
-            self.city.append(host.get('city', 'n/a'))
+            self.banner.append(items['data'])
             self.port.append(items['port'])
-        #print(len(self.ip), len(self.city), len(self.port)) # Verifying list match 
+            self.city.append(host.get('city', 'n/a'))
+            self.domains.append(host.get('domain', 'n/a'))
+            self.asn.append(items['asn'])
+
+        #print(len(self.ip), len(self.city), len(self.port), len(self.domains), len(self.asn)) # Verifying that they all match
 
     def output_format(self):
         table = Table(title="Shodan results")
         table = Table(show_lines = True)
 
         table.add_column("IP", justify="right", style="green", no_wrap=True)
-        table.add_column("City", justify="right", style="red")
-        table.add_column("Port", style="green")
-
-        for index, (a, b, c ) in enumerate(zip(self.ip, self.city, self.port)):
-            table.add_row(str(a), str(b), str(c))
+        table.add_column("Banner", justify="right", style="white")
+        table.add_column("Port", style="red")
+        table.add_column("City", justify="right", style="green")
+        table.add_column("Domains", style="red")
+        table.add_column("ASN", style="Cyan")
+        
+        for index, (a, b, c, d, e, f) in enumerate(zip(self.ip, self.banner, self.port, self.city, self.domains, self.asn)):
+            table.add_row(str(a), str(b), str(c), str(d), str(e), str(f))
 
         console = Console()
         console.print(table)
-
 
 IP1 = diagram(input("Enter the IP address you want to scan: "))
 IP1.shodan_search()
